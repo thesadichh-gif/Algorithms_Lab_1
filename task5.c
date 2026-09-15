12b49063d89bdc0181af76b7190c85208cd77271
Daniil <the.sadichh@gmail.com>
2026-09-15 20:25:42 +0300

    Task completed

--- task5.c
+++ task5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct student {
@@ -6,36 +7,73 @@ struct student {
     char sname[20];
     char facult[20];
     int number;
+    struct student *next;
 };
 
 int main() {
-    struct student students[3];
-    char search[20];
+    struct student *head = NULL;
+    struct student *tail = NULL;
+    char tmpsname[20];
 
-    for (int i = 0; i < 3; i++) {
-        printf("Enter surname:\n");
-        scanf("%19s", students[i].sname);
+    while (1) {
 
-        printf("Enter %s's name:\n", students[i].sname);
-        scanf("%19s", students[i].name);
+        printf("Enter surname (or '*' to stop):\n");
+        scanf("%19s", tmpsname);
+        if (strcmp(tmpsname, "*") == 0) {
+            break;
+        }
 
-        printf("Enter %s's facult:\n", students[i].sname);
-        scanf("%19s", students[i].facult);
+        struct student *newstudent = (struct student*)malloc(sizeof(struct student));
+        strcpy(newstudent->sname, tmpsname);
 
-        printf("Enter %s's number:\n", students[i].sname);
-        scanf("%d", &students[i].number);
+        printf("Enter %s's name:\n", newstudent->sname);
+        scanf("%19s", newstudent->name);
+
+        printf("Enter %s's facult:\n", newstudent->sname);
+        scanf("%19s", newstudent->facult);
+
+        printf("Enter %s's number:\n", newstudent->sname);
+        scanf("%d", &(newstudent->number));
         printf("\n");
+
+        newstudent->next = NULL;
+
+        if (head == NULL) {
+            head = newstudent;
+            tail = newstudent;
+        } else {
+            tail->next = newstudent;
+            tail = newstudent;
+        }
     }
 
-    printf("Enter surname to search: ");
+    char search[20];
+    printf("\nEnter text to search (surname, name, facult, nubmer): ");
     scanf("%19s", search);
 
-    for (int i = 0; i < 3; i++) {
-        if (strcmp(search, students[i].sname) == 0) {
-            printf("Found!\nName: %s\n", students[i].name);
-            printf("Facult: %s\n", students[i].facult);
-            printf("Number: %d\n", students[i].number);
+    struct student *current = head;
+
+    while (current != NULL) {
+        char strnumber[20];
+        sprintf(strnumber, "%d" , current->number);
+        if (strstr(current->sname, search) != NULL ||
+            strstr(current->name, search) != NULL ||
+            strstr(current->facult, search) != NULL ||
+            strstr(strnumber, search) != NULL) {
+
+            printf("Found!\n");
+            printf("Surname: %s\n", current->sname);
+            printf("Name: %s\n", current->name);
+            printf("Facult: %s\n", current->facult);
+            printf("Number: %d\n\n", current->number);
         }
+        current = current->next;
+    }
+    current = head;
+    while (current != NULL) {
+        struct student *temp = current;
+        current = current->next;
+        free(temp);
     }
     return 0;
 }
